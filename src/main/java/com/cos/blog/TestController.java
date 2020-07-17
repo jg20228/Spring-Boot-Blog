a3f3e36f2469d0b6efc7298356e66dcb9e6cbdd3
jg20228 <jg20228@nate.com>
2020-07-17 10:34:52 +0900

    blog 셋팅 끝

---
+++ src/main/java/com/cos/blog/TestController.java
@@ -0,0 +1,13 @@
+package com.cos.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping({"","/"})
+	public String TestIndex() {
+		return "index";
+	}
+}
---
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.cos.blog.repository;
+
+
+public interface UserRepository {
+
+}
